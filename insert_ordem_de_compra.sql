6907828715b2e8695c4e9337e58451819870e9a7
lucas-q-c <99377995+lucas-q-c@users.noreply.github.com>
2023-05-26 19:02:39 -0300

    Add files via upload

---
+++ insert_ordem_de_compra.sql
@@ -0,0 +1,31 @@
+INSERT INTO ORDEM_DE_COMPRA VALUES(1, '01-24-2020', 'FINALIZADA', '20.00', '33.37', 'Gilberto Dantas', 28, 'Pilão', 'Serra Branca', '58580000', 1, 10);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(2, '03-19-2020', 'FINALIZADA', '60.00', '112.27', 'Gilberto Dantas', 28, 'Pilão', 'Serra Branca', '58580000', 2, 9);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(3, '07-12-2020', 'FINALIZADA', '42.54', '27.99', 'Gilberto Dantas', 28, 'Paulista', 'Sao Paulo', '58580000', 3, 8);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(4, '09-01-2020', 'FINALIZADA', '20.67', '12.54', 'alvares cabral', 28, 'Paulista', 'Sao Paulo', '58580000', 4, 7);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(5, '02-06-2021', 'FINALIZADA', '10.00', '22.00', 'rua joao pessoa', 40, 'Tres irmas', 'Rio de Janeiro', '46789000', 5, 6);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(6, '05-14-2021', 'FINALIZADA', '99.99', '67.76', 'rua presidente collor', 40, 'Copacabana', 'Rio de Janeiro', '46789000', 6, 5);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(7, '08-23-2021', 'FINALIZADA', '200.00', '150.00', 'rua presidente prudente', 40, 'Paraisopolis', 'Sao Paulo', '12764000', 7, 4);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(8, '03-07-2022', 'FINALIZADA', '27.89', '60.72', 'Gilberto Dantas', 28, 'Pilão', 'Campina Grande', NULL, 8, 3);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(9, '04-08-2022', 'FINALIZADA', '2.73', '5.34', 'Rua Sao Paulo', 672, 'Catolé', 'Campina Grande', NULL, 9, 2);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(10, '06-09-2022', 'FINALIZADA', '350.00', '270.00', 'Martins Junior', 28, 'Liberdade', 'Campina Grande', NULL, 10, 1);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(11, '10-25-2022', 'FINALIZADA', '12.88', '47.50', 'Rua São João', 200, 'Catolé', 'Campina Grande', NULL, 11, 2);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(12, '12-20-2022', 'FINALIZADA', '33.17', '7.89', 'Rua B', '321', 'Bairro Y', 'Cidade X', '12345671', 11, 2);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(13, '02-17-2023', 'EM TRANSPORTE', '96.29', '20.00', 'Rua A', '123', 'Bairro X', 'Cidade Y', '12345678', 12, 3);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(14, '05-20-2023', 'EM SEPARACAO', NULL, '15.00', 'Rua B', '456', 'Bairro Z', 'Cidade W', '98765432', 13, 6);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(15, '05-20-2023', DEFAULT, '90.00', '40.54', 'Rua C', '789', 'Bairro K', 'Cidade L', '11111111', 14, 9);
+
+INSERT INTO ORDEM_DE_COMPRA VALUES(16, DEFAULT, DEFAULT, '45.88', '23.64', 'Gilberto Dantas', 28, 'Pilão', 'Serra Branca', '58580000', 1, 10);
